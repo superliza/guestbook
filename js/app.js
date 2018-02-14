c8ac569e5e21749c8453546946b8ce32e237e479
superliza <eli_darko@hotmail.com>
2018-02-14 11:23:23 -0600

    se tren elemento por DOM

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//traer elemetos
+var $largeButton = $('#large-button');
+var $mediumButton = $('#medium-button');
+var $smallButton = $('#small-button');
+var $colorTextButton = $('#color-text-button');
+var $backColorButton = $('#back-color-button');
+var $leftButton = $('#left-button');
+var $centerButton = $('#center-button');
+var $rightButton = $('#right-button');
